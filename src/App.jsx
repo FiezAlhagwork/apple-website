8b34108172942f14dbcf1e765bf1b8413f591037
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-03-23 03:08:30 +0300

    finsh section model and model view and finsh components loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+
 import './App.css'
 
 import Hero from './components/Hero'
 import HighLights from './components/HighLights'
+import Model from './components/Model'
 import Navbar from './components/Navbar'
+
 const App = () => {
+
   return (
     <main className='bg-black'>
       <Navbar/>
       <Hero/>
       <HighLights/>
+      <Model/>
     </main>
   )
 }
