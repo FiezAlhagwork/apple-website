098721e5cf56fd49e0d31d78878af9a9de22693d
fiez alhag <148772477+FiezAlhagwork@users.noreply.github.com>
2025-03-27 17:35:25 +0300

    finsh section Footer and finsh Project applw Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Features from './components/Features'
+import Footer from './components/Footer'
 
 import Hero from './components/Hero'
 import HighLights from './components/HighLights'
@@ -19,6 +20,7 @@ const App = () => {
       <Model/>
       <Features/>
       <HowItWork/>
+      <Footer/>
     </main>
   )
 }
